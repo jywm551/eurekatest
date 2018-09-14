c8719193f4b4d7faa46a31e387f45e88fbed43fa
Theodore <jywm551@163.com>
2018-09-15 01:37:32 +0800

    modify

---
+++ eureka-server/src/main/java/com/wecarepet/eurekaserver/EurekaServerApplication.java
@@ -0,0 +1,24 @@
+package com.wecarepet.eurekaserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.http.MediaType;
+import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@SpringBootApplication
+@EnableEurekaServer
+@EnableConfigServer
+public class EurekaServerApplication implements WebMvcConfigurer {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaServerApplication.class);
+    }
+
+    @Override
+    public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
+        configurer.ignoreAcceptHeader(true).defaultContentType(MediaType.APPLICATION_JSON);
+    }
+}
